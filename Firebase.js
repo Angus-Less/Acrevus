415fdff40301b4665e196c756f8acf8447603b12
Angus Moore <nubytube@gmail.com>
2021-08-11 16:08:49 +1000

    Made firebase operational for injection

---
+++ Firebase.js
@@ -0,0 +1,15 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDrD-udDqxDehOayG9U95fAeAYlwFwIYME",
+    authDomain: "acrevus-4bb74.firebaseapp.com",
+    projectId: "acrevus-4bb74",
+    storageBucket: "acrevus-4bb74.appspot.com",
+    messagingSenderId: "435255014159",
+    appId: "1:435255014159:web:6aa1f1b4ea0af1360a447a",
+    measurementId: "G-33TKV57R3K"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
