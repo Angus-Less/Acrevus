b17ce8795a88a494a5c642be28dddbd71a23870a
NhatHuyNguyen <nhathuy.nguyen@gmail.com>
2021-09-23 17:56:49 +0700

    add back missing files

---
+++ background.js
@@ -0,0 +1,76 @@
+// on tab load
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete' || changeInfo.url) {
+        get_domain_tags(); 
+    }
+  })
+
+// duplicate code ftw
+function get_domain_tags() {
+    /**
+     * Returns an array of all the domains in the HTML page the user is looking at.
+     * 
+     * Return:
+     *      Array containing all websites listed on a given google search.
+     */
+     var tab_title = '';
+     var currentPage = 0; // This should be incremented in states of 2 [0, 2, 4, etc...]
+     //var numberOfSites = 20; // TODO: FIX THIS SO THAT WE CAN RE-ALLOCATE IT.
+     var list_of_sites = [];
+
+    //Get the number of searches available.
+    // Go through the page and get the cite tags from the google search.
+    
+    chrome.tabs.query({active: true}, function(tabs) {
+        var tab = tabs[0];
+        tab_title = tab.title;
+        var numberOfSites = 0;
+        chrome.tabs.query({active: true}, function(tabs) {
+            var tab = tabs[0];
+            tab_title = tab.title;
+            chrome.tabs.executeScript(tab.id, {
+            code: `document.querySelectorAll("cite").length`
+            }, function(results) {
+                
+                numberOfSites = results;
+
+                // Highlights all <cite> tags in red + add icons
+                for (var i = 0; i < numberOfSites; i++) {
+                    var id = chrome.runtime.id
+                    // only add if it hasn't been added yet
+                    chrome.tabs.executeScript(tab.id, {
+                        code: `var loaded = document.querySelectorAll(".icon_acrevus${i}").length != 0; 
+                                var domain = String(document.querySelectorAll("cite")[${i}].textContent).split(" ")[0]; 
+                                var icon_path = (domain.includes("uq")) ? 'tick_icon.png' : 'cross_icon.png';
+                                if (!loaded) { 
+                                    //document.querySelectorAll("cite")[${i}].style.backgroundColor = "red";
+                                    document.querySelectorAll("cite")[${i}].innerHTML += "<img class='icon_acrevus${i}' src = chrome-extension://${id}/img/" + String(icon_path) + " style='width:24px;height:24px;vertical-align: middle;margin-left:8px;'>"
+                                }`
+                    }, function(results) {
+                        // Do nothing but specify callback function.
+                    }); 
+                }
+
+                // Exceute the script modification.
+                for (var i = 0; i < numberOfSites; i++) {
+                    if (i % 2 == 0 || i == 0) {
+                        chrome.tabs.executeScript(tab.id, {
+                            code: `document.querySelectorAll("cite")[${i}].textContent`
+                        }, function(results) {
+                            // Modifies the extension's HTML.
+                            let subDomain = String(results).split(" ")[0]
+                            document.getElementById('test').innerHTML += "<p>Domain list: "+ `${i} ` + subDomain +"</p>";
+                        }); 
+                    }
+                    
+                }
+            });
+        });    
+    });
+
+
+ 
+    
+   
+    
+}
