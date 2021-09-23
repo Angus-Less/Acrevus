d953dd0161fd9da8bed1e1f6ba2c891551ddf57b
DavidMcGovern <58420920+DavidMcGovern@users.noreply.github.com>
2021-09-23 16:39:29 +1000

    I redid it

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ function check_website(site) {
         })
 }
 
+document.getElementById("descButton").addEventListener("click", get_description);
 function get_description() {
     var ratingDict = {
         null : "unknown",
@@ -60,52 +61,15 @@ function get_description() {
         "1" : "trustworthy"
     }
 
-    check_website("bbc.co.uk").then(rating => {
+    check_website(document.getElementById("siteInput").value).then(rating => {
         console.log(rating)
+
+
+        document.getElementById("description").innerHTML =
+            document.getElementById("siteInput").value + " is " +
+            ratingDict[rating];
     })
 }
 
-get_description()
 
 
-
-/*
-// Write data
-// This code is from : https://firebase.google.com/docs/firestore/query-data/get-data
-var citiesRef = db.collection("cities");
-
-citiesRef.doc("SF").set({
-    name: "San Francisco", state: "CA", country: "USA",
-    capital: false, population: 860000,
-    regions: ["west_coast", "norcal"] });
-citiesRef.doc("LA").set({
-    name: "Los Angeles", state: "CA", country: "USA",
-    capital: false, population: 3900000,
-    regions: ["west_coast", "socal"] });
-citiesRef.doc("DC").set({
-    name: "Washington, D.C.", state: null, country: "USA",
-    capital: true, population: 680000,
-    regions: ["east_coast"] });
-citiesRef.doc("TOK").set({
-    name: "Tokyo", state: null, country: "Japan",
-    capital: true, population: 9000000,
-    regions: ["kanto", "honshu"] });
-citiesRef.doc("BJ").set({
-    name: "Beijing", state: null, country: "China",
-    capital: true, population: 21500000,
-    regions: ["jingjinji", "hebei"] });
-
-    var docRef = db.collection("cities").doc("SF");
-
-// Read data.
-// This code is from : https://firebase.google.com/docs/firestore/query-data/get-data
-docRef.get().then(function(doc) {
-    if (doc.exists) {
-        console.log("Document data:", doc.data());
-    } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-    }
-}).catch(function(error) {
-    console.log("Error getting document:", error);
-});*/
