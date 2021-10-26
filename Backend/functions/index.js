ee9878904c8c75519aceea93510d3118f6e284e7
DavidMcGovern <58420920+DavidMcGovern@users.noreply.github.com>
2021-10-26 21:58:39 +1000

    summarising descriptions now working?

--- Backend/functions/index.js
+++ Backend/functions/index.js
@@ -5,30 +5,36 @@ const OpenAI = require("openai-api");
 
 const openai = new OpenAI(functions.config().openai.access_token);
 
-// Shorten URL written to /links/{linkID}.
-exports.shortenUrl = functions.firestore.document("/openai/{word}").onCreate(
-    async (snap) => {
-      const document = snap.data();
-      const OpenAIinput = document.OpenAIinput;
-      const response = await openai.complete({
-        engine: "davinci",
-        prompt: OpenAIinput,
-        maxTokens: 5,
-        temperature: 0.9,
-        topP: 1,
-        presencePenalty: 0,
-        frequencyPenalty: 0,
-        bestOf: 1,
-        n: 1,
-        stream: false,
-        stop: ["\n", "testing"],
-      });// @ts-ignore
+// Summarise description of a site when made
+exports.summariseDescription =
+  functions.firestore.document("/openai/{word}").onCreate(
+      async (snap) => {
+        const document = snap.data();
+        const OpenAIinput = document.desc;
+        const response = await openai.complete({
+          engine: "davinci-instruct-beta",
+          prompt:
+            "I was asked to summarise the below paragraph " +
+            "for an external audience:\n\"\"\"" +
+            OpenAIinput +
+            "\n\"\"\"\nI briefly summarised it as such:\n\"\"\"",
+          maxTokens: 100,
+          temperature: 0.9,
+          topP: 1,
+          presencePenalty: 0,
+          frequencyPenalty: 0,
+          bestOf: 1,
+          n: 1,
+          stream: false,
+          stop: ["\"\"\"", "\n"],
+        });// @ts-ignore
 
-      const expandedResponse = response.data;
+        const expandedResponse = response.data;
 
-      return snap.ref.set({
-        input: OpenAIinput,
-        APIresponse: expandedResponse,
-        output: expandedResponse.choices[0].text,
+        return snap.ref.set({
+          name: document.name,
+          rating: document.rating,
+          longDesc: OpenAIinput,
+          desc: expandedResponse.choices[0].text,
+        });
       });
-    });
